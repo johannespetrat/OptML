9519aa094ed86395e914e4e79477f0a8cff6e012
johannes <johannes.petrat@capgemini.com>
2018-01-19 09:37:48 +0000

    fixing readme issue

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='optml',
 
-    version='0.2.1',
+    version='0.2.2',
 
     description='A library for hyperparameter optimization of ML models',
     long_description=long_description,
