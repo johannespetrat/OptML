eb05b81a482ea76a40af039bbb57c27cb922731a
johannes <johannes.petrat@capgemini.com>
2018-01-19 09:50:44 +0000

    fixing relative imports for python 3

--- optml/__init__.py
+++ optml/__init__.py
@@ -1,5 +1,5 @@
 from optml import models
-from optml import bayesian_optimizer
+import optml.bayesian_optimizer
 from optml import genetic_optimizer
 from optml import gridsearch_optimizer
 from optml import random_search
